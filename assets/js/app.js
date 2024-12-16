bb9ed6103c4eb0ff9d972c38222ef277d31ecd0a
IRyan00 <eymas.ryan@gmail.com>
2024-12-16 14:16:52 +0100

    articleID complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// MAP
 let myMap = L.map("map").setView([43.493933099243286, -1.4931438459626258], 5);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
@@ -5,21 +6,28 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20
 }).addTo(myMap);
 
-let sfo_marker1 = L.marker([48.857232, 2.352586]).addTo(myMap);
-let sfo_marker2 = L.marker([52.519012, 13.377164]).addTo(myMap);
+let parisPin = L.marker([48.857232, 2.352586]).addTo(myMap);
+let berlinPin = L.marker([52.519012, 13.377164]).addTo(myMap);
+let praguePin = L.marker([50.087064, 14.417694]).addTo(myMap);
+let bledPin = L.marker([46.367754, 14.111616]).addTo(myMap);
+let manausPin = L.marker([-3.112044, -60.007570]).addTo(myMap);
+let almatyPin = L.marker([43.240727, 76.923102]).addTo(myMap);
 
+parisPin.bindPopup('<b><span class="popupLink" data-id="1">Paris, France</span></b>');
+berlinPin.bindPopup('<b><span class="popupLink" data-id="2">Berlin, Allemagne</span></b>');
+praguePin.bindPopup('<b><span class="popupLink" data-id="3">Prague, Tchéquie</span></b>');
+bledPin.bindPopup('<b><span class="popupLink" data-id="4">Bled, Slovénie</span></b>');
+manausPin.bindPopup('<b><span class="popupLink" data-id="5">Manaus, Brésil</span></b>');
+almatyPin.bindPopup('<b><span class="popupLink" data-id="6">Almaty, Kazakhstan</span></b>');
 
-sfo_marker1.bindPopup('<b><span id="popupLink1">Paris</span></b>');
-sfo_marker2.bindPopup('<b><span id="popupLink2">Berlin</span></b>');
-
-// Ajoute un écouteur d'événement sur la carte pour détecter l'ouverture d'un popup
 myMap.on('popupopen', () => {
-
-	// Récupère l'élément avec l'ID 'popupLink' à l'intérieur du popup
-	document.getElementById('popupLink1').addEventListener('click', () => {
-
-		// Redirige l'utilisateur vers une URL spécifique lorsqu'il clique sur le lien
-		window.location.href = "";
+	document.querySelectorAll('.popupLink').forEach(link => {
+		link.addEventListener('click', (event) => {
+			const id = event.target.getAttribute('data-id');
+			if (id) {
+				window.location.href = `/templates/articleID.html?id=${id}`;
+			}
+		});
 	});
 });
 
--- assets/js/articleID.js
+++ assets/js/articleID.js
@@ -1,46 +1,45 @@
-let myParam = new URLSearchParams(window.location.search);
-let myID = myParam.get("id");
+const articles = [
+	{ id: "1", img: '../assets/images/paris.jpg', title: 'Paris, France', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque beatae ipsum aspernatur excepturi totam exercitationem incidunt blanditiis quis laboriosam veniam at, qui alias enim, fugiat consequuntur aut adipisci aliquid quas omnis repellendus mollitia et ratione. Quos error laborum repudiandae consequatur repellat autem nihil neque voluptatum provident rem cupiditate reiciendis, quam iste ipsum incidunt porro ullam? Quas eveniet repudiandae sapiente necessitatibus. Unde reprehenderit nemo, explicabo possimus incidunt soluta veritatis! Earum suscipit sunt iusto aliquid rerum placeat nobis perspiciatis consequatur, eum quaerat cum accusamus alias nisi beatae quae in reiciendis quod facere necessitatibus ipsam sapiente. Eligendi aut cum iusto nam veniam?', desc2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque beatae ipsum aspernatur excepturi totam exercitationem incidunt blanditiis quis laboriosam veniam at, qui alias enim, fugiat consequuntur aut adipisci aliquid quas omnis repellendus mollitia et ratione. Quos error laborum repudiandae consequatur repellat autem nihil neque voluptatum provident rem cupiditate reiciendis, quam iste ipsum incidunt porro ullam? Quas eveniet repudiandae sapiente necessitatibus. Unde reprehenderit nemo, explicabo possimus incidunt soluta veritatis! Earum suscipit sunt iusto aliquid rerum placeat nobis perspiciatis consequatur, eum quaerat cum accusamus alias nisi beatae quae in reiciendis quod facere necessitatibus ipsam sapiente. Eligendi aut cum iusto nam veniam?" },
+	{ id: "2", img: '../assets/images/berlin.jpg', title: 'Berlin, Allemagne', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque beatae ipsum aspernatur excepturi totam exercitationem incidunt blanditiis quis laboriosam veniam at, qui alias enim, fugiat consequuntur aut adipisci aliquid quas omnis repellendus mollitia et ratione. Quos error laborum repudiandae consequatur repellat autem nihil neque voluptatum provident rem cupiditate reiciendis, quam iste ipsum incidunt porro ullam? Quas eveniet repudiandae sapiente necessitatibus. Unde reprehenderit nemo, explicabo possimus incidunt soluta veritatis! Earum suscipit sunt iusto aliquid rerum placeat nobis perspiciatis consequatur, eum quaerat cum accusamus alias nisi beatae quae in reiciendis quod facere necessitatibus ipsam sapiente. Eligendi aut cum iusto nam veniam?', desc2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque beatae ipsum aspernatur excepturi totam exercitationem incidunt blanditiis quis laboriosam veniam at, qui alias enim, fugiat consequuntur aut adipisci aliquid quas omnis repellendus mollitia et ratione. Quos error laborum repudiandae consequatur repellat autem nihil neque voluptatum provident rem cupiditate reiciendis, quam iste ipsum incidunt porro ullam? Quas eveniet repudiandae sapiente necessitatibus. Unde reprehenderit nemo, explicabo possimus incidunt soluta veritatis! Earum suscipit sunt iusto aliquid rerum placeat nobis perspiciatis consequatur, eum quaerat cum accusamus alias nisi beatae quae in reiciendis quod facere necessitatibus ipsam sapiente. Eligendi aut cum iusto nam veniam?" },
+	{ id: "3", img: '../assets/images/prague.jpg', title: 'Prague, Tchéquie', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque beatae ipsum aspernatur excepturi totam exercitationem incidunt blanditiis quis laboriosam veniam at, qui alias enim, fugiat consequuntur aut adipisci aliquid quas omnis repellendus mollitia et ratione. Quos error laborum repudiandae consequatur repellat autem nihil neque voluptatum provident rem cupiditate reiciendis, quam iste ipsum incidunt porro ullam? Quas eveniet repudiandae sapiente necessitatibus. Unde reprehenderit nemo, explicabo possimus incidunt soluta veritatis! Earum suscipit sunt iusto aliquid rerum placeat nobis perspiciatis consequatur, eum quaerat cum accusamus alias nisi beatae quae in reiciendis quod facere necessitatibus ipsam sapiente. Eligendi aut cum iusto nam veniam?', desc2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque beatae ipsum aspernatur excepturi totam exercitationem incidunt blanditiis quis laboriosam veniam at, qui alias enim, fugiat consequuntur aut adipisci aliquid quas omnis repellendus mollitia et ratione. Quos error laborum repudiandae consequatur repellat autem nihil neque voluptatum provident rem cupiditate reiciendis, quam iste ipsum incidunt porro ullam? Quas eveniet repudiandae sapiente necessitatibus. Unde reprehenderit nemo, explicabo possimus incidunt soluta veritatis! Earum suscipit sunt iusto aliquid rerum placeat nobis perspiciatis consequatur, eum quaerat cum accusamus alias nisi beatae quae in reiciendis quod facere necessitatibus ipsam sapiente. Eligendi aut cum iusto nam veniam?" },
+	{ id: "4", img: '../assets/images/bled.jpg', title: 'Bled, Slovénie', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque beatae ipsum aspernatur excepturi totam exercitationem incidunt blanditiis quis laboriosam veniam at, qui alias enim, fugiat consequuntur aut adipisci aliquid quas omnis repellendus mollitia et ratione. Quos error laborum repudiandae consequatur repellat autem nihil neque voluptatum provident rem cupiditate reiciendis, quam iste ipsum incidunt porro ullam? Quas eveniet repudiandae sapiente necessitatibus. Unde reprehenderit nemo, explicabo possimus incidunt soluta veritatis! Earum suscipit sunt iusto aliquid rerum placeat nobis perspiciatis consequatur, eum quaerat cum accusamus alias nisi beatae quae in reiciendis quod facere necessitatibus ipsam sapiente. Eligendi aut cum iusto nam veniam?', desc2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque beatae ipsum aspernatur excepturi totam exercitationem incidunt blanditiis quis laboriosam veniam at, qui alias enim, fugiat consequuntur aut adipisci aliquid quas omnis repellendus mollitia et ratione. Quos error laborum repudiandae consequatur repellat autem nihil neque voluptatum provident rem cupiditate reiciendis, quam iste ipsum incidunt porro ullam? Quas eveniet repudiandae sapiente necessitatibus. Unde reprehenderit nemo, explicabo possimus incidunt soluta veritatis! Earum suscipit sunt iusto aliquid rerum placeat nobis perspiciatis consequatur, eum quaerat cum accusamus alias nisi beatae quae in reiciendis quod facere necessitatibus ipsam sapiente. Eligendi aut cum iusto nam veniam?" },
+	{ id: "5", img: '../assets/images/manaus.webp', title: 'Manaus, Brésil', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque beatae ipsum aspernatur excepturi totam exercitationem incidunt blanditiis quis laboriosam veniam at, qui alias enim, fugiat consequuntur aut adipisci aliquid quas omnis repellendus mollitia et ratione. Quos error laborum repudiandae consequatur repellat autem nihil neque voluptatum provident rem cupiditate reiciendis, quam iste ipsum incidunt porro ullam? Quas eveniet repudiandae sapiente necessitatibus. Unde reprehenderit nemo, explicabo possimus incidunt soluta veritatis! Earum suscipit sunt iusto aliquid rerum placeat nobis perspiciatis consequatur, eum quaerat cum accusamus alias nisi beatae quae in reiciendis quod facere necessitatibus ipsam sapiente. Eligendi aut cum iusto nam veniam?', desc2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque beatae ipsum aspernatur excepturi totam exercitationem incidunt blanditiis quis laboriosam veniam at, qui alias enim, fugiat consequuntur aut adipisci aliquid quas omnis repellendus mollitia et ratione. Quos error laborum repudiandae consequatur repellat autem nihil neque voluptatum provident rem cupiditate reiciendis, quam iste ipsum incidunt porro ullam? Quas eveniet repudiandae sapiente necessitatibus. Unde reprehenderit nemo, explicabo possimus incidunt soluta veritatis! Earum suscipit sunt iusto aliquid rerum placeat nobis perspiciatis consequatur, eum quaerat cum accusamus alias nisi beatae quae in reiciendis quod facere necessitatibus ipsam sapiente. Eligendi aut cum iusto nam veniam?" },
+	{ id: "6", img: '../assets/images/almaty.jpg', title: 'Almaty, Kazakhstan', desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque beatae ipsum aspernatur excepturi totam exercitationem incidunt blanditiis quis laboriosam veniam at, qui alias enim, fugiat consequuntur aut adipisci aliquid quas omnis repellendus mollitia et ratione. Quos error laborum repudiandae consequatur repellat autem nihil neque voluptatum provident rem cupiditate reiciendis, quam iste ipsum incidunt porro ullam? Quas eveniet repudiandae sapiente necessitatibus. Unde reprehenderit nemo, explicabo possimus incidunt soluta veritatis! Earum suscipit sunt iusto aliquid rerum placeat nobis perspiciatis consequatur, eum quaerat cum accusamus alias nisi beatae quae in reiciendis quod facere necessitatibus ipsam sapiente. Eligendi aut cum iusto nam veniam?', desc2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cumque beatae ipsum aspernatur excepturi totam exercitationem incidunt blanditiis quis laboriosam veniam at, qui alias enim, fugiat consequuntur aut adipisci aliquid quas omnis repellendus mollitia et ratione. Quos error laborum repudiandae consequatur repellat autem nihil neque voluptatum provident rem cupiditate reiciendis, quam iste ipsum incidunt porro ullam? Quas eveniet repudiandae sapiente necessitatibus. Unde reprehenderit nemo, explicabo possimus incidunt soluta veritatis! Earum suscipit sunt iusto aliquid rerum placeat nobis perspiciatis consequatur, eum quaerat cum accusamus alias nisi beatae quae in reiciendis quod facere necessitatibus ipsam sapiente. Eligendi aut cum iusto nam veniam?" },
+];
 
-console.log(myID);
-
-
-let APIKEY = "20c7592d";
-let URL = "./articles.js" + myID;
-
-console.log(URL);
-
-
-function newCard() {
-
-	fetch(URL)
-		.then(response => response.json())
-		.then(data => {
-			let myResult = data;
-			console.log(myResult);
-
-			document.getElementById('contentInfos').innerHTML += `
-				<div class="my-5 row justify-content-center align-items-center gap-5">
-					<div class="col-5">
-						<img src="${data.Poster}" class="card-img-top rounded-4" alt="${data.Title} de ${data.Year}">
-					</div>
-
-					<div class="col-5">
-						<h1 class="mt-3 mb-5">${data.Title} (${data.Year})</h1>
-						<p class="my-3">Release: ${data.Released}</p>
-						<p class="my-3">Genre: ${data.Genre}</p>
-						<p class="my-3">Runtime: ${data.Runtime}</p>
-						<p class="my-3">Rating: ${data.Rated}</p>
-						<p class="my-3">Metascore: ${data.Metascore}</p>
-						<p class="my-3">Director: ${data.Director}</p>
-						<p class="my-3">Writers: ${data.Writer}</p>
-						<p class="my-3">Actors: ${data.Actors}</p>
-						<p class="my-3">Plot: ${data.Plot}</p>
-						<button type="button" class="btn btn-success">Buy</button>
-					</div>
-				</div>
-			`
-		})
-		.catch(error => console.error("error", error));
+function getIdFromUrl() {
+	const params = new URLSearchParams(window.location.search);
+	return params.get("id");
 }
 
-newCard()
+
+function displayContent() {
+	const id = getIdFromUrl();
+	const content = document.getElementById("content");
+
+	const article = articles.find((item) => item.id === id);
+
+	if (article) {
+		content.innerHTML = `
+			<h1 class="h1 text-center py-5 fw-bold">${article.title}</h1>
+			<p class="col-10 mx-auto">${article.desc}</p>
+			<p class="col-10 mx-auto">${article.desc2}</p>
+			<div class="d-flex justify-content-center col-8 mx-auto">
+				<img src="${article.img}" class="img-fluid my-5 rounded-3">
+       		</div>
+        `;
+	} else {
+		content.innerHTML = "<p>Cet article est probablement supprimé ou en cours d'écriture...</p>";
+	}
+}
+
+displayContent()
+
+
+// DARKMODE
+const body = document.querySelector('body')
+const check = document.querySelector('#checkbox')
+
+check.addEventListener('change', function () {
+	body.classList.toggle('dark')
+});
